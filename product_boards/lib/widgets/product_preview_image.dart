c4807a6d154a21284081acd58b09785272188614
lineSence <paralelepipid654@gmail.com>
2026-08-20 16:11:22 +0300

    Fix preview image placeholder after legacy widget cleanup

--- product_boards/lib/widgets/product_preview_image.dart
+++ product_boards/lib/widgets/product_preview_image.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+
 import '../models/product_preview.dart';
 import '../screens/image_diagnostics_screen.dart';
-import 'product_card.dart';
 
 class ProductPreviewImage extends StatelessWidget {
   const ProductPreviewImage({super.key, required this.preview, this.fit = BoxFit.cover});
@@ -61,4 +61,15 @@ class ProductPreviewImage extends StatelessWidget {
       errorBuilder: (_, __, ___) => const PlaceholderImage(),
     );
   }
+}
+
+class PlaceholderImage extends StatelessWidget {
+  const PlaceholderImage({super.key});
+
+  @override
+  Widget build(BuildContext context) => Container(
+        color: const Color(0xffecece9),
+        alignment: Alignment.center,
+        child: const Text('🐈', style: TextStyle(fontSize: 52)),
+      );
 }
