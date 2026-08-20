ad0eea93b9c864f1eb65cb96f12ac280678afd80
lineSence <paralelepipid654@gmail.com>
2026-08-20 16:06:31 +0300

    test: fix package import

--- product_boards/test/archive_item_test.dart
+++ product_boards/test/archive_item_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:product_boards/models/archive_item.dart';
+import 'package:pinzon/models/archive_item.dart';
 
 defaultItem() => ArchiveItem(
       id: '1',
